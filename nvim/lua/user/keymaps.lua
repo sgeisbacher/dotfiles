ea22c3a0da3b1366653ff1853a7100097c3124c5
Stefan Geisbacher <stefan.geisbacher@gmail.com>
2023-12-07 23:06:24 +0100

    nvim-lua: setup keymaps

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,77 @@
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+--
+--
+--
+-- GENERAL
+keymap("", "<space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+--
+--
+--
+--
+-- NORMAL MODE --
+keymap("n", "<leader>ttc", ":cclose<CR>", opts) -- close tooltip pane
+
+-- search
+keymap("n", "n", "nzz", opts) -- center next search-result
+keymap("n", "N", "Nzz", opts) -- center prev search-result
+keymap("n", "<leader><leader>", ":nohlsearch<CR>", opts) -- reset search-term highlighting
+
+-- yank to register b
+keymap("n", "yb", '"byy', opts)
+keymap("n", "bp", '"bP', opts)
+keymap("n", "bP", '"bP', opts)
+
+-- yank to register c
+keymap("n", "yc", '"cyy', opts)
+keymap("n", "cp", '"cP', opts)
+keymap("n", "cP", '"cP', opts)
+
+-- split planes
+keymap("n", "<leader>vs", ":wincmd v<CR>", opts) -- split windows vertical
+keymap("n", "<leader>ss", ":wincmd s<CR>", opts) -- split windows horizontal
+keymap("n", ">", "<C-w>>", opts) -- grow split pane
+keymap("n", "<", "<C-w><", opts) -- shrink split pane
+
+-- navigation
+-- in buffer
+keymap("n", "[", "{", opts) -- jump one paragraph up
+keymap("n", "]", "}", opts) -- jump one paragraph down
+
+-- in window
+keymap("n", "<m-h>", ":bp<CR>", opts) -- jump to next buffer
+keymap("n", "<m-l>", ":bn<CR>", opts) -- jump to prev buffer
+
+keymap("n", "<C-h>", "<C-w>h", opts) -- jump to left pane
+keymap("n", "<C-l>", "<C-w>l", opts) -- jump to right pane
+keymap("n", "<C-k>", "<C-w>k", opts) -- jump to upper pane
+keymap("n", "<C-j>", "<C-w>j", opts) -- jump to lower pane
+
+keymap("n", "<S-Up>", ":resize -2<CR>", opts) -- shrink pane vertical
+keymap("n", "<S-Down>", ":resize +2<CR>", opts) -- grow pane vertical
+keymap("n", "<S-Left>", ":vertical resize -2<CR>", opts) -- shrink pane horizontal
+keymap("n", "<S-Right>", ":vertical resize +2<CR>", opts) -- grow pane horizontal
+
+--
+--
+--
+--
+--
+-- VISUAL MODE --
+-- yanking
+keymap("v", "<leader>y", '"+y', opts) -- yank to os clipboard
+keymap("v", "yb", '"byy', opts) -- yank to register b
+keymap("v", "yc", '"cyy', opts) -- yank to register c
+keymap("v", "p", '"_dP', opts) -- yank to register c
+
+-- window navigation
+keymap("v", "<Up>", "<nop>", opts)
+keymap("v", "<Down>", "<nop>", opts)
+
+-- search
+keymap("v", "//", "y/\\V<C-R>=escape(@\",'/\\')<CR><CR>", opts) -- search for selected text
